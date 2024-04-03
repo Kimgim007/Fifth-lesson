1614bba0d8e9234af3c97d5d68e8d7820d19004c
maks.kep.2000@mail.ru <maks.kep.2000@mail.ru>
2024-04-03 23:17:33 +0300

    Add project files.

---
+++ Fifth lesson/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+
+namespace Fifth_lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 1.
+            //string firstName = "AAA";
+            //string secondName = "BBB";
+
+            //string fullName = firstName + " " + secondName;
+
+            //string fullNameSecond = string.Concat(firstName," ", secondName);
+
+            //string fullNameThree = "{0} {1}";
+
+            //Console.WriteLine(fullName);
+            //Console.WriteLine(fullNameSecond);
+
+            //Console.WriteLine($"{firstName} {secondName}");
+            //Console.WriteLine(string.Format(fullNameThree,firstName,secondName));
+
+
+            //2.
+
+            //string firstName = "qqq";
+            //string lastName = "Ktoto";
+
+            //string fullName = "";
+
+            //if (firstName.Length > 3)
+            //{
+            //    fullName = firstName.Remove(3);
+
+            //}
+            //else
+            //{
+            //    fullName = firstName;
+            //}
+            //fullName = string.Concat(fullName, "***");
+            //Console.WriteLine(fullName);
+
+            //3.
+
+            //string firstName = "Maksim";
+            //string lastName = "    ";
+
+            //if(string.IsNullOrEmpty(lastName))
+            //{
+            //    Console.WriteLine($"{firstName[0]}.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{firstName[0]}.{lastName[0]}.");
+            //}
+
+            //4.
+
+            //StringBuilder line = new StringBuilder();
+           
+            //while (true)
+            //{
+            //    Console.WriteLine("Введите слово");
+
+            //    line.Append(Console.ReadLine());
+
+            //    Console.Clear();
+            //    Console.WriteLine(line);
+
+            //    Console.WriteLine("Продолжить введите +, закончить -");
+
+            //    if(Console.ReadLine() == "-")
+            //    {
+            //        break;
+            //    }
+            //}
+            
+
+        }
+    }
+}
